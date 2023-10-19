1e3d8d5153dd888de43c5433a184c484b708f4e5
Johanes10 <jcordobamauna@gmail.com>
2023-10-19 15:50:11 -0500

    Sprint Johan

---
+++ index.js
@@ -0,0 +1,46 @@
+const contenedor = document.getElementById("contenedorCards")
+// const titulo = document.createElement("h1")
+// titulo.innerHTML = "Hola Johan"
+// contenedor.appendChild(titulo)
+const upcoming = document.getElementById("contenedorUpcomingEvents")
+const events = data.events
+
+
+{/* <div class="card" style="width: 18rem;">
+<img src="./Recursos_Amazing_Events_Task_1/books.jpg" class="card-img-top" alt="Books Event">
+<div class="card-body">
+  <h5 class="card-title">Book Fair</h5>
+  <p class="card-text text-truncate">Explore a world of literature at our Book Fair. Discover new stories and authors.</p>
+  <div class="d-flex justify-content-between">
+    <p>$5</p>
+    <a href="#" class="btn btn-outline-dark">Details</a>
+  </div> */}
+for (let event of events) {
+  const card = document.createElement("div")
+  card.classList.add("card")
+  card.style.width = "18rem"
+  card.innerHTML = `<img src=${event.image} class="card-img-top h-50" alt="Books Event">
+                                <div class="card-body">
+                                <h5 class="card-title">${event.name}</h5 >
+                                <p class="card-text text-truncate">${event.description}</p>
+                                <div class="d-flex justify-content-between">
+                                <p>$${event.price}</p>
+                                <a href="#" class="btn btn-outline-dark">Details</a>`
+  contenedor.appendChild(card)
+}
+
+for (let event of events) {
+  if (event.date >= "2023-01-01") {
+    const card = document.createElement("div")
+    card.classList.add("card")
+    card.style.width = "18rem"
+    card.innerHTML = `<img src=${event.image} class="card-img-top h-50" alt="Books Event">
+                        <div class="card-body">
+                        <h5 class="card-title">${event.name}</h5 >
+                        <p class="card-text text-truncate">${event.description}</p>
+                        <div class="d-flex justify-content-between">
+                        <p>$${event.price}</p>
+                        <a href="#" class="btn btn-outline-dark">Details</a>`
+    upcoming.appendChild(card)
+  }
+}
