f9ca5238c08d6b0fd73f8e6fa75e002fc9309421
Johanes10 <jcordobamauna@gmail.com>
2023-10-19 16:36:06 -0500

    Amazing Events

---
+++ pasEvents.js
@@ -0,0 +1,18 @@
+const pastEvents = document.getElementById("contenedorPastEvents")
+const events = data.events
+
+for (let event of events) {
+    if (event.date < "2023-01-01") {
+        const card = document.createElement("div")
+        card.classList.add("card")
+        card.style.width = "18rem"
+        card.innerHTML = `<img src=${event.image} class="card-img-top h-50" alt="Books Event">
+                        <div class="card-body">
+                        <h5 class="card-title">${event.name}</h5 >
+                        <p class="card-text text-truncate">${event.description}</p>
+                        <div class="d-flex justify-content-between">
+                        <p>$${event.price}</p>
+                        <a href="#" class="btn btn-outline-dark">Details</a>`
+        pastEvents.appendChild(card)
+    }
+}
